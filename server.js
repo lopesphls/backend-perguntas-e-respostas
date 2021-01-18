cb7d6d3ec88b0f1de3aaeb57e23c20e2ef159977
Pedro Lopes <lopesphls@gmail.com>
2021-01-18 14:23:40 -0300

    fontend

--- server.js
+++ server.js
@@ -3,29 +3,14 @@ const app = express();
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let exibirMsg = false;
+app.get("/",(req, res) => {
+    res.render("index");
+});
 
-    let produtos = [
-        {nome: "Doritos",preco: 3.14},
-        {nome: "Coca-cola",preco:5},
-        {nome: "Leite",preco:1.45},
-        {nome: "Carne", preco:15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: 8040,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
 });
 
 app.listen(3000,()=>{
