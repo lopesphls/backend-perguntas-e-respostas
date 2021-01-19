3a66d1eae2e219b9849d1dfd27acda2f3ef2d420
Pedro Lopes <lopesphls@gmail.com>
2021-01-19 10:02:54 -0300

    Incercao sequelize

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão com Banco concluida');
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/",(req, res) => {
     res.render("index");
@@ -12,6 +26,12 @@ app.get("/",(req, res) => {
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
 });
+
+app.post("/savequestion",(req, res) => {
+    const titulo = req.body.titulo;
+    const descricao = req.body.descricao;
+    res.send(`Formulário recebido com titulo ${titulo} e com descrição ${descricao}`);
+})
 
 app.listen(3000,()=>{
     console.log("App rodando!");
